8d402c2e6a5282ef0121ce2a52ff4c4d2522bc24
Sergio <sergi_xavier01@hotmail.com>
2016-01-14 02:31:19 -0500

    Mejoramiento IHM general del juego y agregacion de fondos de pantalla para el juego

--- Capstone/public_html/js/main.js
+++ Capstone/public_html/js/main.js
@@ -20,7 +20,7 @@ $(document).on('focus', '.panel-footer input.chat_input', function (e) {
 });
 $(document).on('click', '#new_chat', function (e) {
     var size = $( ".chat-window:last-child" ).css("margin-left");
-     size_total = parseInt(size) + 400;
+     size_total = parseInt(size) + 200;
     alert(size_total);
     var clone = $( "#chat_window_1" ).clone().appendTo( ".container" );
     clone.css("margin-left", size_total);
@@ -29,53 +29,4 @@ $(document).on('click', '.icon_close', function (e) {
     //$(this).parent().parent().parent().parent().remove();
     $( "#chat_window_1" ).remove();
 });
-
-//Para la animacion de los porcentajes del circulo
-window.onload = function() {
-//selecciono la clase javascript -->
-    var javascript = document.querySelector('.javascript');
-    new EasyPieChart(javascript, {
-        //<!-- activo la animación y establezco su duración a un segundo -->
-        animate: ({ duration: 1000, enabled: true }),
-        //<!-- aumento la longitud de las lineas de la gráfica -->
-        scaleLength:10,
-        //<!-- aumento el tamaño de la gráfica -->
-        size:150,
-        //<!-- añado el número del porcentaje que se muestra en el span -->
-        onStep: function(from, to, percent) {
-            this.el.children[0].innerHTML = Math.round(percent);
-        }
-    });
-    var html5 = document.querySelector('.html5');
-    new EasyPieChart(html5, {
-        animate: ({ duration: 2000, enabled: true }),
-        scaleLength:10,
-        size:150,
-        barColor:'#FF8000',
-        onStep: function(from, to, percent) {
-            this.el.children[0].innerHTML = Math.round(percent);
-        }
-    });
-    var css = document.querySelector('.css');
-    new EasyPieChart(css, {
-        animate: ({ duration: 3000, enabled: true }),
-        scaleLength:10,
-        size:150,
-        barColor:'#58ACFA',
-        onStep: function(from, to, percent) {
-            this.el.children[0].innerHTML = Math.round(percent);
-        }
-    });
-    var php = document.querySelector('.php');
-    new EasyPieChart(php, {
-        animate: ({ duration: 3000, enabled: true }),
-        scaleLength:10,
-        size:150,
-        barColor:'#1C1C1C',
-        onStep: function(from, to, percent) {
-            this.el.children[0].innerHTML = Math.round(percent);
-        }
-    });
-}
- 
 
